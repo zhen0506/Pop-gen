df708a2a1fdb216b3fed89ddd1b9a0f755034c1e
zhen <fanzhen199156@gmail.com>
2022-01-10 14:22:24 -0500

    Deleted testing file

--- allchecks.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-def check_reboot()
-	"""Returns True if the computer has a pending reboot."""
-	return os.path.exist("/run/reboot-required")
-def main():
-	pass
-
-main()
